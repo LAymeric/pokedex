6f929517d4c0985b10021faa1dccf874355e05ce
LAymeric <aymeric.larvet@gmail.com>
2019-09-27 11:08:38 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+     <pokemon v-for="poke in info.data.results" :pokemon="poke"/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import pokemon from './pokemon';
+
+export default {
+  name: 'HelloWorld',
+  components: { pokemon },
+  data () {
+    return {
+      msg: 'test',
+      info:{
+        data :{
+          results :[]
+        }
+      }
+    }
+  },
+  mounted () {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/?limit=150')
+      .then(response => (this.info = response))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/pokemon.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="pokemon">
+        <h3>{{this.pokemon.name }}</h3>
+  </div>
+</template>
+<script>
+export default {
+   props: ['pokemon']
+};
+</script>
