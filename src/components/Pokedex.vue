9e30405da9d4bd5c50fac139fcfc6187335fbb04
LAymeric <aymeric.larvet@gmail.com>
2019-09-29 20:55:13 +0200

    format

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="pokedex">
     <h1>{{ msg }}</h1>
-     <Pokemon v-for="poke in info.data.results" v-bind:poke="poke" 
-    v-bind:key="poke.id" :pokemon="poke"/>
+    <Pokemon
+      v-for="poke in info.data.results"
+      v-bind:poke="poke"
+      v-bind:key="poke.id"
+      :pokemon="poke"
+    />
   </div>
-
 </template>
 
 <script>
-import axios from 'axios';
-import Pokemon from './Pokemon';
+import axios from 'axios'
+import Pokemon from './Pokemon'
 
 export default {
-  name: 'Pokedex', 
+  name: 'Pokedex',
   components: { Pokemon },
   data () {
     return {
@@ -28,9 +31,6 @@ export default {
     axios
       .get('https://pokeapi.co/api/v2/pokemon/?limit=151')
       .then(response => (this.info = response))
-  },
+  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-
--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-        <h2>{{this.pokemonDetails.id}}</h2>
+    <h2>{{this.pokemonDetails.id}}</h2>
   </div>
 </template>
 <script>
 export default {
-  name: 'PokemonDetails', 
-   props: ['pokemonDetails']
-};
+  name: 'PokemonDetails',
+  props: ['pokemonDetails']
+}
 </script>
